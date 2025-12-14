983ae67984e1cdd9ecfadb1731a998d88a83ce4a
zwyu-ai <zhongwei-yu@outlook.com>
2025-12-14 12:55:22 +0800

    BO applications in HER, HEA, and BH datasets
    The (first-version) code to apply HEBO and standard BO (GP-LCB) to some scientific datasets.
    Thanks: Zhenzhi Tan (THU), Qi Yang (THU), Zikai Xie (UCST), Alex Maraval (Huawei), Rasul Tutunov (Huawei), and Haitham B. A. (Huawei).

---
+++ HEA/oracle.py
@@ -0,0 +1,75 @@
+from autogluon.tabular import TabularPredictor
+from autogluon.tabular import TabularDataset
+import pandas as pd
+
+
+class ObjectiveWrapper:
+
+    def __init__(self, black_box_function,
+                 input_length: int = None,
+                 input_columns: list = None,
+                 input_list=False):
+
+        self.black_box_function = black_box_function
+        assert not (input_length is None and input_columns is None), 'Must provide input length or input column names!'
+
+        if input_columns is None:
+            input_columns = []
+            for i in range(input_length):
+                input_columns.append('x'+str(i))
+
+        self.input_columns = input_columns
+        self.input_list = input_list
+
+    def __call__(self, *args, **kwargs):
+
+        if args != ():
+            if 'predict' not in dir(self.black_box_function):
+                return self.black_box_function(*args)
+            else:
+                to_predict = pd.DataFrame(args[0])
+                if to_predict.shape[-1] == 1:
+                    to_predict = to_predict.T
+                to_predict.columns = self.input_columns
+                return list(self.black_box_function.predict(to_predict))
+
+        input_data = list(kwargs.values())
+        if 'predict' not in dir(self.black_box_function):
+            if not self.input_list:
+                return self.black_box_function(*input_data)
+            else:
+                return self.black_box_function(input_data)
+        else:
+            to_predict = pd.DataFrame(input_data).T
+            to_predict.columns = self.input_columns
+            return list(self.black_box_function.predict(to_predict))[0]
+
+
+def get_data():
+    data = pd.read_excel('./data/oracle_data.xlsx')
+    data = data[['Co', 'Fe', 'Mn', 'V', 'Cu', 'target']]
+    return data
+
+
+def train_model():
+    train_data = get_data()
+    data_train_X = TabularDataset(train_data)
+    predictor = TabularPredictor(label='target').fit(data_train_X)
+    return predictor
+
+
+def get_model():
+    model_dir = './AutogluonModels/ag-20251209_102532'
+    return TabularPredictor.load(model_dir)
+
+
+class Oracle:
+    def __init__(self):
+        self.model = get_model()
+
+    def __call__(self, x):
+        return self.model.predict(x)
+
+
+if __name__ == "__main__":
+    train_model()
